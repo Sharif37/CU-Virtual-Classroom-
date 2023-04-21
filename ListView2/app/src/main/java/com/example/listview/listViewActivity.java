22d830f8cc23f71425672fa6e871d0bb347c71a4
sharif37 <sharif.csecu@gmail.com>
2023-04-21 23:04:11 +0600

    added new features

--- ListView2/app/src/main/java/com/example/listview/listViewActivity.java
+++ ListView2/app/src/main/java/com/example/listview/listViewActivity.java
@@ -3,12 +3,34 @@ package com.example.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class listViewActivity extends AppCompatActivity {
 
+
+    ListView list ;
+    String[] subject;
+    ArrayAdapter<String> adapter ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
+
+        list=findViewById(R.id.list_view);
+        subject=getResources().getStringArray(R.array.data);
+        adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,subject);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String sub=parent.getItemAtPosition(position).toString();
+                Toast.makeText(listViewActivity.this, sub+"", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
